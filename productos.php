2ed8040c478f79ac58035b86cda5f84a5c118f5e
David Prieto <deybitsds1@gmail.com>
2024-07-26 17:09:44 -0500

    5:09 implementado barra de busqueda

--- productos.php
+++ productos.php
@@ -30,8 +30,9 @@
              
              <!-- DERECHA -->
 
-                <a href="index.html" class="nav-link">
-                    <img src="tornillo.png" alt="Icono" class="icono">
+                
+                <a href="index.html" class="nav-link-icono">
+                    <img src="tornillo.png" alt="Icono" class="icono_tornillo">
                 </a>
         
                 <a href="logout.html" class="nav-link btn-salir">Salir</a>
@@ -73,22 +74,23 @@
 
 
         <!-- barra busqueda -->
-        <div class="group">
-            <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
-            <input placeholder="Search" type="search" class="input">
-            
-        </div>
+        <form action="correcto.php" method="post" class="input-group mb-3 mt-3">
 
-    <!-- PHP Script -->
-    <?php
-        $a = 8;
-        $b = 3;
-        echo "Valor de a: ", $a, " y el valor de b: ", $b, "<br>";
-        if ($a < $b) {
-            echo "$a es menor que $b";
-        } else {
-            echo "$a no es menor que $b";
-        }
-    ?>
+        <div class="group_search">
+        <svg viewBox="0 0 24 24" aria-hidden="true" class="icon_search">
+          <g>
+            <path
+              d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+            ></path>
+          </g>
+        </svg>
+        <input class="input_search" type="search" name="codigo_buscado" placeholder="Ingrese código de barra" />
+        </div>
+    
+        </form>
+
+
+
+
 </body>
 </html>
